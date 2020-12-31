b8947fdbe54ac26241cc4582fed345e6e6831612
Diarte Jeffcoat <diartesjeffcoat@icloud.com>
2020-12-31 02:51:16 -0500

    Completed main cup functionality with the small cups

--- script.js
+++ script.js
@@ -42,6 +42,18 @@ function updateMainCup() {
     if (fullCups === 0) {
         percentage.style.visibility = 'hidden'
         percentage.style.height = 0
+    } else {
+        percentage.style.visibility = 'visible'
+        percentage.style.height = `${fullCups / totalCups * 330}px`
+        percentage.innerText = `${fullCups / totalCups * 100}%`
+    }
+
+    if (fullCups === totalCups) {
+        remainder.style.visibility = 'hidden'
+        remainder.style.height = 0
+    } else {
+        remainder.style.visibility = 'visible'
+        liters.innerText = `${2 - (250 * fullCups / 1000)} L`
     }
 
 }
